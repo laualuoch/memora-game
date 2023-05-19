6ecf45ee0a7f8efaa77e3242a47d8dfa1ca21f79
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-05-19 21:41:04 +0300

    useState to determine card value when flipped and if matched on flip

--- src/App.js
+++ src/App.js
@@ -11,6 +11,20 @@ const Card = ( {onClick}) => {
 }
 
 const FlipCardGame = () => {
+
+  const [cards, setCards] = useState ([
+    { id: 1, value: 'bee-cap', flipped: false, matched: false },
+    { id: 2, value: 'bee-romper', flipped: false, matched: false },
+    { id: 3, value: 'bee-skirt', flipped: false, matched: false },
+    { id: 4, value: 'bee-shoes', flipped: false, matched: false },
+    { id: 5, value: 'bee-shirt', flipped: false, matched: false },
+    { id: 6, value: 'bee-cap', flipped: false, matched: false },
+    { id: 7, value: 'bee-romper', flipped: false, matched: false },
+    { id: 8, value: 'bee-skirt', flipped: false, matched: false },
+    { id: 9, value: 'bee-shoes', flipped: false, matched: false },
+    { id: 10, value: 'bee-shirt', flipped: false, matched: false },
+  ]);
+
   return (
     <>
      <div className="App">
@@ -71,7 +85,7 @@ const FlipCardGame = () => {
 const GameManager = () => {
   return (
     <>
-    <Display />
+    <Display task = "Moves" clickAction = {clickStat}/>
     <Display />
     <Display />
     <Button />
