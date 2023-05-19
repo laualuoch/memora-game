c9482169c93850c8584d81da29984b97bd2d34f4
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-05-19 18:10:57 +0300

    update card appearance in a row

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import logo from './images/logo.svg';
+import card from './images/card.svg';
 import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className='App-header'>
         <p>
-          Flip card to Play!
+            Flip card to Play!
         </p>
       </header>
+      <body className="App-body">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <img src={card} class="img-fluid" alt="Card One"></img>
+            </div>
+            <div class="col">
+              <img src={card} class="img-fluid" alt="Card Two"></img>
+            </div>
+            <div class="col">
+              <img src={card} class="img-fluid" alt="Card Three"></img>
+            </div>
+            <div class="col">
+              <img src={card} class="img-fluid" alt="Card Four"></img>
+            </div>
+            <div class="col">
+              <img src={card} class="img-fluid" alt="Card Five"></img>
+            </div>
+          </div>
+        </div>
 
-      <body>
-        /* The card rows go header
-        Will create a cardBoard 
-        function to hold all the 10 cards later. 
-        */
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <img src={card} class="img-fluid" alt="Card Ten"></img>
+            </div>
+            <div class="col">
+              <img src={card} class="img-fluid" alt="Card Nine"></img>
+            </div>
+            <div class="col">
+              <img src={card} class="img-fluid" alt="Card Eight"></img>
+            </div>
+            <div class="col">
+              <img src={card} class="img-fluid" alt="Card Seven"></img>
+            </div>
+            <div class="col">
+              <img src={card} class="img-fluid" alt="Card Six"></img>
+            </div>
+          </div>
+        </div>
       </body>
     </div>
   );
